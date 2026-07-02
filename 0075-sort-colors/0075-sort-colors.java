7a4938890d916c2912b45ad9bba9d8511d494586
garimayadav20 <168345481+garimayadav20@users.noreply.github.com>
2026-07-03 02:28:49 +0530

    Time: 3 ms (14.18%), Space: 43.9 MB (10.58%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,16 +1,22 @@
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int n = nums.length;
+//         for (int j = 0; j < n-1; j++) {
+
+//             for (int i = 1; i < n - j; i++) {
+//                 if (nums[i] < nums[i - 1]) {
+//                     int temp = nums[i];
+//                     nums[i] = nums[i - 1];
+//                     nums[i - 1] = temp;
+//                 }
+
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        for (int j = 0; j < n-1; j++) {
-
-            for (int i = 1; i < n - j; i++) {
-                if (nums[i] < nums[i - 1]) {
-                    int temp = nums[i];
-                    nums[i] = nums[i - 1];
-                    nums[i - 1] = temp;
-                }
-
-            }
-        }
+        Arrays.sort(nums);
     }
 }
